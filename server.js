f359ebee831e01842df010a366715e882e390cea
Janikow <160335817+Janikow@users.noreply.github.com>
2026-01-14 09:45:44 -0600

    redirect to main menu

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const io = new Server(server, {
 app.use(express.static(path.join(__dirname, "chat/public")));
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "chat/public", "index.html"));
+  res.sendFile(path.join(__dirname, "index.html"));
 });
 
 
