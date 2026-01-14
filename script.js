a3fc44aa019422e0d94a5868904bbf7e703540ce
Janikow <160335817+Janikow@users.noreply.github.com>
2026-01-14 14:34:59 -0600

    reworked button functions

---
+++ script.js
@@ -0,0 +1,7 @@
+
+// Game hyperlink button converter
+document.querySelectorAll(".game-button").forEach(btn => {
+    btn.addEventListener("click", () => {
+      window.location.href = btn.dataset.link;
+    });
+  });
